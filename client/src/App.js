6f9fc8c259cd55cfbbd73293ccb634d8511dfbc5
Misty-Dev <markojau@gmail.com>
2022-05-03 16:22:56 -0300

    Header, NewTransfers and TransferList

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { getWeb3, getWallet } from './utils.js';
+import Header from './components/Header.js';
+import NewTransfer from './components/NewTransfer.js';
+import { TransferList } from './components/TransferList.js';
+
 function App() {
-	return <div> Multisig Wallet Dapp</div>;
+	const [web3, setWeb3] = useState(undefined);
+	const [accounts, setAccounts] = useState(undefined);
+	const [wallet, setWallet] = useState(undefined);
+	const [approvers, setApprovers] = useState([]);
+	const [quorum, setQuorum] = useState(undefined);
+	const [transfers, setTransfers] = useState([]);
+
+	useEffect(() => {
+		const init = async () => {
+			const web3 = getWeb3();
+			const accounts = await web3.eth.getAccounts();
+			const wallet = await getWallet(web3);
+			const approvers = await wallet.methods.getApprovers().call();
+			const quorum = await wallet.methods.quorum().call();
+			const transfers = await wallet.methods.getTransfers().call();
+			setWeb3(web3);
+			setAccounts(accounts);
+			setWallet(wallet);
+			setApprovers(approvers);
+			setQuorum(quorum);
+			setTransfers(transfers);
+		};
+		init();
+	}, []);
+
+	if (
+		typeof web3 === 'undefined' ||
+		typeof accounts === 'undefined' ||
+		typeof wallet === 'undefined' ||
+		approvers.length === 0 ||
+		typeof quorum === 'undefined'
+	) {
+		return <div>Loading...</div>;
+	}
+
+	const createTransfer = (transfer) => {
+		wallet.methods
+			.createTransfer(transfer.amount, transfer.to)
+			.send({ from: accounts[0] });
+	};
+
+	const approveTransfer = (transferId) => {};
+
+	return (
+		<div>
+			Multisig Wallet Dapp
+			<Header approvers={approvers} quorum={quorum} />
+			<NewTransfer createTransfer={createTransfer} />
+			<TransferList transfers={transfers} />
+		</div>
+	);
 }
 
 export default App;
